645827a21206fd5217329b1c952f4f25c80e1ca0
wangyalijas <1114920411@qq.com>
2018-05-25 12:48:39 +0800

    test3

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-const express = require('express')
-const app = express()
-app.post('/webhook', (req, res) => console.log(req))
-app.listen(4000, () => console.log('Example app listening on port 4000!'))
+var http = require('http')
+const shell = require('shelljs')
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/webhook', secret: '123456' })
+
+const projectHandler = (event, action) => {
+  const project = event.payload.repository.name // 提交的仓库名字
+  console.log(project);
+  const branch = event.payload.ref
+  if (projects.includes(project)) {
+    console.log(new Date())
+    shell.exec(`sh deploy.sh`, function (code, stdout, stderr) {
+      console.log(new Date(), 'Exit code:', code)
+      // console.log(new Date(), 'Program output:', stdout)
+      console.log(new Date(), '执行完毕！错误信息：？', stderr)
+    })
+
+  }
+}
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(4000)
+
+
+handler.on('push', function (event) {
+  projectHandler(event, 'push')
+})
